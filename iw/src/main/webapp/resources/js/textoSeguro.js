98213dde6d9934d52c69f5479547c0b2cb562ceb
J-888 <josemigueltajuelo@gmail.com>
2016-02-02 22:09:22 +0100

    Arreglo validacion error raro

--- iw/src/main/webapp/resources/js/textoSeguro.js
+++ iw/src/main/webapp/resources/js/textoSeguro.js
@@ -1,27 +1,34 @@
 	function palabraSeguro(palabra) {
-		return palabra.matches("^[a-zA-Z0-9_ ]*$");
+		//return palabra.match("^[a-zA-Z0-9_ ]*$");
+		return /^[a-zA-Z0-9_ ]*$/.test(palabra);
 	};
 
 	function descripcionSeguro (descripcion) {
-		return descripcion.matches("^[a-zA-Z0-9-_,._ ]*$");
+		//return descripcion.match("^[a-zA-Z0-9-_,._ ]*$");
+		return /^[a-zA-Z0-9-_,._ ]*$/.test(descripcion);
 	};
 
 	function direccionSeguro (direccion) {
-		return direccion.matches("^[a-zA-Z0-9-_,._ ]*$");
+		//return direccion.match("^[a-zA-Z0-9-_,._ ]*$");
+		return /^[a-zA-Z0-9-_,._ ]*$/.test(direccion);
 	};
 
-	function horarioSeguro (direccion) {
-		return direccion.matches("^[0-9-]*$");
+	function horarioSeguro (horario) {
+		//return direccion.match("^[0-9-]*$");
+		return /^[0-9-]*$/.test(horario);
 	};
 
 	function telefonoSeguro (telefono) {
-		return telefono.matches("^[0-9]{9}$");
+		//return telefono.match("^[0-9]{9}$");
+		return /^[0-9]{9}$/.test(telefono);
 	};
 
 	function fechaSeguro (fecha) {
-		return fecha.matches("^[0-9 -:]*$");
+		//return fecha.match("^[0-9 -:]*$");
+		return /^[0-9 -:]*$/.test(fecha);
 	};
 
 	function emailSeguro(email) {
-		return email.matches("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@" + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
+		//return email.match("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@" + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
+		return /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/.test(email);
 	};
